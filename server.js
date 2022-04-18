349b7faff6deb5669eea1a81d063e2cf4c285546
escowin <edwin@escowinart.com>
2022-04-18 14:52:43 -0500

    11.2.6 | app.post success

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const req = require('express/lib/request');
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming POST (string or array) data. converts data to key/value pairs. pairs accessed in req.body
+// extended: true | informs server that there may exist nested sub-array data. looks DEEP & parses through post data correctly.
+app.use(express.urlencoded({ extended: true }));
+// parse incoming json data. above & below middleware functions need to be set up every time a server is created to accept POST data
+app.use(express.json())
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -36,6 +42,14 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    console.log(body);
+    // function code
+    
+    // return finished code to post route for response
+    return body;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -51,6 +65,13 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.send(404);
   }
+});
+
+// create route | listens for POST requests. client requesting the server to accept data
+app.post('/api/animals', (req, res) => {
+    // where incoming content will be
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
