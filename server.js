fa006177285e078fa427cb41caae228a1446f587
escowin <edwin@escowinart.com>
2022-04-18 15:30:17 -0500

    11.2.7 | snapshot

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const req = require('express/lib/request');
-const { animals } = require('./data/animals');
 const fs = require('fs');
-// module built into the Node.js API that provides utilities for working with file and directory paths
 const path = require('path');
+const express = require('express');
+const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming POST (string or array) data. converts data to key/value pairs. pairs accessed in req.body
-// extended: true | informs server that there may exist nested sub-array data. looks DEEP & parses through post data correctly.
+
 app.use(express.urlencoded({ extended: true }));
-// parse incoming json data. above & below middleware functions need to be set up every time a server is created to accept POST data
-app.use(express.json())
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -46,36 +42,29 @@ function findById(id, animalsArray) {
 }
 
 function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animalsArray.push(animal);
-    // synchronous version of fs.writeFile()
-    // writes animals.json in ./data.
-    fs.writeFileSync(
-        path.join(__dirname, './data/animals.json'),
-        // converts JS array data to JSON
-        // null prevents editing existing data. 2 creates white space between values
-        JSON.stringify({ animals: animalsArray }, null, 2)
-    );
-
-    // return finished code to post route for response
-    return body;
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
 }
 
-// data validation | user-inputed
 function validateAnimal(animal) {
-    if (!animal.name || typeof animal.name !== 'string') {
-        return false;
-    }
-    if (!animal.species || typeof animal.species !== 'string') {
-        return false;
-    }
-    if (!animal.diet || typeof animal.diet !== 'string') {
-        return false;
-    }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-        return false;
-    }
-    return true;
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -95,21 +84,17 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
-// create route | listens for POST requests. client requesting the server to accept data
 app.post('/api/animals', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
 
-    // if any data in req.body is incorrect, send 400 error
-    // 400 error | user error
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('animal improperly formatted.');
-    } else {
-        const animal = createNewAnimal(req.body, animals);
-        res.json(animal);
-    }
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
-
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
