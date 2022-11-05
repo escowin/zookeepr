00ae1cc53dd7055209bec8338e158b8516e6a293
escowin <edwin@escowinart.com>
2022-11-05 17:10:05 -0500

    redux

--- routes/apiRoutes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const animalRoutes = require("../apiRoutes/animalRoutes");
-const zookepperRoutes = require("../apiRoutes/zookeeperRoutes");
-
-router.use(animalRoutes);
-router.use(zookepperRoutes);
-
-module.exports = router;
